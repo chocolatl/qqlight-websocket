9af5e4d0338cd5edd2e3916a64f20bcd196efd3b
chocolatl <choclatl@outlook.com>
2018-09-14 20:51:42 +0800

    0.1.0
    - 添加 接口.获取群名片
    - 添加 接口.上传图片
    - 使用文件保存插件日志

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ const char* PLUGIN_INFO =
 	"pluginBrief="
 		"Enable you to use QQLight API in any language you like via WebSocket.\r\n\r\n"
 		"GitHub:\r\nhttps://github.com/Chocolatl/qqlight-websocket;\r\n"
-	"pluginVersion=0.0.0;\r\n"
+	"pluginVersion=0.1.0;\r\n"
 	"pluginSDK=3;\r\n"
 	"pluginAuthor=Chocolatl;\r\n"
 	"pluginWindowsTitle=;"
