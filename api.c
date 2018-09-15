047b2da2b4bbba768477dfcd87671c47c913ee86
chocolatl <choclatl@outlook.com>
2018-09-15 21:35:38 +0800

    重构DLL函数加载代码

--- api.c
+++ api.c
@@ -16,26 +16,26 @@ int loadQQLightAPI(void) {
         return -1;
     }
 
-    if(
-        (QL_getPluginPath = GetProcAddress(libHandle, "Api_GetPath")) == NULL ||
-        (QL_printLog = GetProcAddress(libHandle, "Api_SendLog")) == NULL ||
-        (QL_sendMessage = GetProcAddress(libHandle, "Api_SendMsg")) == NULL ||
-        (QL_withdrawMessage = GetProcAddress(libHandle, "Api_DeleteMsg")) == NULL ||
-        (QL_getFriendList = GetProcAddress(libHandle, "Api_GetFriendList")) == NULL ||
-        (QL_addFriend = GetProcAddress(libHandle, "Api_AddFriend")) == NULL ||
-        (QL_deleteFriend = GetProcAddress(libHandle, "Api_DeleteFriend")) == NULL ||
-        (QL_getGroupList = GetProcAddress(libHandle, "Api_GetGroupList")) == NULL ||
-        (QL_getGroupMemberList = GetProcAddress(libHandle, "Api_GetGroupMemberList")) == NULL ||
-        (QL_addGroup = GetProcAddress(libHandle, "Api_AddGroup")) == NULL ||
-        (QL_quitGroup = GetProcAddress(libHandle, "Api_QuitGroup")) == NULL ||
-        (QL_getGroupCard = GetProcAddress(libHandle, "Api_GetGroupCard")) == NULL ||
-        (QL_uploadImage = GetProcAddress(libHandle, "Api_UpLoadPic")) == NULL
-    ) {
-        FreeLibrary(libHandle);
-        return -1;
-    }
+    #define GET_DLL_FUNC(dname, ename) if((dname = GetProcAddress(libHandle, ename)) == NULL) goto laodFuncError;
+
+    GET_DLL_FUNC(QL_getPluginPath, "Api_GetPath");
+    GET_DLL_FUNC(QL_printLog, "Api_SendLog");
+    GET_DLL_FUNC(QL_sendMessage, "Api_SendMsg");
+    GET_DLL_FUNC(QL_withdrawMessage, "Api_DeleteMsg");
+    GET_DLL_FUNC(QL_getFriendList, "Api_GetFriendList");
+    GET_DLL_FUNC(QL_addFriend, "Api_AddFriend");
+    GET_DLL_FUNC(QL_deleteFriend, "Api_DeleteFriend");
+    GET_DLL_FUNC(QL_getGroupList, "Api_GetGroupList");
+    GET_DLL_FUNC(QL_getGroupMemberList, "Api_GetGroupMemberList");
+    GET_DLL_FUNC(QL_addGroup, "Api_AddGroup");
+    GET_DLL_FUNC(QL_quitGroup, "Api_QuitGroup");
+    GET_DLL_FUNC(QL_getGroupCard, "Api_GetGroupCard");
+    GET_DLL_FUNC(QL_uploadImage, "Api_UpLoadPic");
 
     isLoaded = true;
-    
     return 0;
+
+    laodFuncError:
+    FreeLibrary(libHandle);
+    return -1;
 }
